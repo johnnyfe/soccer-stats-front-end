a6dc13ecc474c050f24250a0099fa914eca5535e
johnny <johnny1194@hotmail.com>
2021-08-09 22:07:49 -0400

    fixing update form soccer teams

--- src/components/SoccerTeamContainer.js
+++ src/components/SoccerTeamContainer.js
@@ -5,15 +5,38 @@ import SoccerTeam from './SoccerTeam'
 function SoccerTeamContainer() {
 
     const [soccerTeams, setSoccerTeams] = useState([])
+    
+    //READ
 
     useEffect(() => {
         fetch(BASE_URL + 'soccer_teams')
         .then(res => res.json())
         .then(setSoccerTeams)
-    }, [])
+    }, []);
 
     function populateSoccerTeams() {
-        return soccerTeams.map(soccer_team => <SoccerTeam soccer_team={soccer_team} deleteSoccerTeam={deleteSoccerTeam} updateSoccerTeam={updateSoccerTeam} key={soccer_team.id}/> )
+        return soccerTeams.map((soccer_team) => (
+        <SoccerTeam soccer_team={soccer_team} deleteSoccerTeam={deleteSoccerTeam} updateSoccerTeam={updateSoccerTeam} key={soccer_team.id}/> ));
+    }
+
+    //UPDATE
+    
+    function updateSoccerTeam(soccer_team) {
+        fetch(BASE_URL + 'soccer_teams/' + soccer_team.id, {
+            method: "PATCH",
+            body: JSON.stringify(soccer_team),
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+              },
+        });
+        const newSoccerTeams = soccerTeams.map((st) =>{
+            if (st.id === soccer_team.id) {
+                st = soccer_team;
+            }
+            return st
+        })
+        setSoccerTeams(newSoccerTeams)
     }
 
     function deleteSoccerTeam(soccer_team){
@@ -22,19 +45,6 @@ function SoccerTeamContainer() {
         })
         const newSoccerTeams = soccerTeams.filter(st => st.id!== soccer_team.id)
         setSoccerTeams(newSoccerTeams)
-    }
-
-    function updateSoccerTeam(soccer_team) {
-        fetch(BASE_URL + 'soccer_teams/' + soccer_team.id, {
-            method: "UPDATE",
-            body: JSON.stringify(soccer_team)
-        })
-        const newSoccerTeams = soccerTeams.map(st =>{
-            if (st.id === soccer_team.id) {
-                st = soccer_team
-            }
-        })
-        setSoccerTeams([...newSoccerTeams])
     }
 
     return (
