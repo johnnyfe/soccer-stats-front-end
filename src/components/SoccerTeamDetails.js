a8e78140b6c232bfd75eafc20f588dcaf30eadec
johnny <johnny1194@hotmail.com>
2021-08-10 01:34:44 -0400

    adding players to soccer team details

--- src/components/SoccerTeamDetails.js
+++ src/components/SoccerTeamDetails.js
@@ -20,14 +20,29 @@ function SoccerTeamDetails() {
 
     return (
         <div>
-            {soccerTeam && <>
-            <p>Soccer Team: {soccerTeam.name}</p>
-            <p>Date Founded: {soccerTeam.founded}</p>
-            <p>Nationality: {soccerTeam.country}</p>
-            <p>Coach: {soccerTeam.manager}</p>
-            <img src={soccerTeam.img_url} alt={soccerTeam.name}></img>
+            {soccerTeam && (
+            <>
+                <p>Soccer Team: {soccerTeam.name}</p>
+                <p>Date Founded: {soccerTeam.founded}</p>
+                <p>Nationality: {soccerTeam.country}</p>
+                <p>Coach: {soccerTeam.manager}</p>
+                <img src={soccerTeam.img_url} alt={soccerTeam.name}></img>
+                {soccerTeam.soccer_players.map( player => (
+                    <div>
+                        <p>Name: {player.name}</p>
+                        <p>Age: {player.age}</p>
+                        <p>Matches: {player.matches}</p>
+                        <p>Goals: {player.goals}</p>
+                        <p>Assists: {player.assists}</p>
+                        <p>Yellow Cards: {player.yellow_cards}</p>
+                        <p>Red Cards: {player.red_cards}</p>
+                        <p>Position: {player.position}</p>
+                        <p>Country: {player.country}</p>
+                        <img src={player.img_url} alt={player.name}></img>
+                    </div>
+            ))}
             </>
-            }
+            )}
         </div>
     );
 }
